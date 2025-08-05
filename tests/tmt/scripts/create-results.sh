7a0e5ada34b31f09e94bba1d29dc6f045195c7be
Anton Misskii <amisskii@redhat.com>
2025-08-05 09:16:19 +0200

    test: condition, exit code Signed-off-by: Anton Misskii <amisskii@redhat.com>

--- tests/tmt/scripts/create-results.sh
+++ tests/tmt/scripts/create-results.sh
@@ -3,15 +3,27 @@ set -euo pipefail
 
 cd "$TMT_TEST_DATA"
 
-cp ../output.txt .
 cp "$TMT_TREE/tests/playwright/output/junit-results.xml" .
 
+if [ "$1" -eq 0 ]; then 
 cat <<EOF > ./results.yaml
-- name: /tests/tmt/e2e-smoke
+- name: /tests/$2
   result: pass
   note: 
-    - "Playwright test executed and results referenced directly from original location."
+    - "Playwright tests passed."
   log:
     - output.txt
     - junit-results.xml
 EOF
+
+elif [ "$1" -eq 255 ]; then 
+cat <<EOF > ./results.yaml
+- name: /tests/$2
+  result: fail
+  note: 
+    - "Playwright tests failed."
+  log:
+    - output.txt
+    - junit-results.xml
+EOF
+fi
