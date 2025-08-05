884e499ed03833d46aae74b6399c46a1129e964e
Anton Misskii <amisskii@redhat.com>
2025-08-05 10:52:54 +0200

    test: modify script Signed-off-by: Anton Misskii <amisskii@redhat.com>

--- tests/tmt/scripts/create-results.sh
+++ tests/tmt/scripts/create-results.sh
@@ -3,28 +3,22 @@ set -euo pipefail
 
 cd "$TMT_TEST_DATA"
 
-cp  -r "$TMT_TREE/tests/playwright/output" .
+cp -r "$TMT_TREE/tests/playwright/output" .
 
-if [ "$1" -eq 0 ]; then 
-cat <<EOF > ./results.yaml
-- name: /tests/$2
-  result: pass
-  note: 
-    - "Playwright tests passed."
-  log:
-    - ../output.txt
-    - output
-EOF
-
-elif [ "$1" -eq 255 ]; then 
-
-cat <<EOF > ./results.yaml
-- name: /tests/$2
-  result: fail
-  note: 
-    - "Playwright tests failed."
-  log:
-    - ../output.txt
-    - output
-EOF
+if [ "$1" -eq 0 ]; then
+  result="pass"
+  note="Playwright tests passed."
+else
+  result="fail"
+  note="Playwright tests failed."
 fi
+
+cat <<EOF > ./results.yaml
+- name: /tests/$2
+  result: $result
+  note: 
+    - "$note"
+  log:
+    - ../output.txt
+    - output
+EOF
